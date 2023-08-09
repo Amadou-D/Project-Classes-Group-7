aa769d00535322c15cdaff0458c32469849d24d6
sh1vrm <sanketh.mekala@edu.sait.ca>
2023-08-08 20:52:17 -0600

    final commit

--- doctor.py
+++ doctor.py
@@ -1,7 +1,8 @@
+# Create Doctor class
 class Doctor:
 
     #Doctor object constructor 
-    def init(self,doctor_id,name,specialization, working_time, qualification,room_number):
+    def __init__(self,doctor_id,name,specialization, working_time, qualification,room_number):
         self.doctor_id = doctor_id
         self.name = name
         self.specialization = specialization
@@ -12,41 +13,41 @@ class Doctor:
     # Getters
     def get_doctor_id(self):
         return self.doctor_id
-
+    
     def get_name(self):
         return self.name
-
+    
     def get_specialization(self):
         return self.specialization
-
+    
     def get_working_time(self):
         return self.working_time
-
+    
     def get_qualification(self):
         return self.qualification
-
+    
     def get_room_number(self):
         return self.room_number 
-
-#Setters
+    
+    # Setters
     def set_doctor_id(self,new_id):
         self.doctor_id = new_id
-
+    
     def set_name(self,new_name): 
         self.name = new_name
-
+    
     def set_specialization(self, new_specialization):
         self.specialization = new_specialization
-
+    
     def set_working_time(self,new_working_time):
         self.working_time = new_working_time
-
+    
     def set_qualification(self, new_qualification):
         self.qualification = new_qualification
-
+    
     def set_room_number(self, new_room_number):
         self.room_number = new_room_number
 
     # String method to return a string representation of the doctor object 
-    def str(self):
-        return f"{self.doctorid}{self.name}{self.specialization}{self.workingtime}{self.qualification}_{self.room_number}"
+    def __str__(self):
+        return f"{self.doctor_id}_{self.name}_{self.specialization}_{self.working_time}_{self.qualification}_{self.room_number}"
